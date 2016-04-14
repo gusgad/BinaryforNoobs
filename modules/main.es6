920021ce0f8cfa51117d61b9d919cd096f1efaa8
Huseyn Gadirov <huseyngadirov@gmail.com>
2016-04-14 02:05:44 -0800

    Fix goal setting

--- modules/main.es6
+++ modules/main.es6
@@ -1,25 +1,10 @@
 require('./title.es6');
 require('./hide-decimals.es6');
+let sup = require('./goal-set.es6');
 
-let goalSet = {
-    init: function() {
-        this.domCache();
-        this.eventBinder();
-    },
-    
-    domCache: function() {
-        this.goalSet = document.getElementById('goalSet');
-        this.goalInput = document.getElementById('goalValue');
-        this.goal = document.getElementById('goal');
-    },
-    
-    eventBinder: function() {
-        this.goalSet.addEventListener('click', this.setGoal.bind(this));
-    },
-    
-    setGoal: function() {
-        this.goal.innerHTML = this.goalInput.value;
-    }
-};
+alert(sup.setGoal);
 
-goalSet.init();
+
+
+
+
--- modules/main.js
+++ modules/main.js
@@ -3,26 +3,6 @@
 
 require('./title.es6');
 require('./hide-decimals.es6');
+var sup = require('./goal-set.es6');
 
-var goalSet = {
-    init: function init() {
-        this.domCache();
-        this.eventBinder();
-    },
-
-    domCache: function domCache() {
-        this.goalSet = document.getElementById('goalSet');
-        this.goalInput = document.getElementById('goalValue');
-        this.goal = document.getElementById('goal');
-    },
-
-    eventBinder: function eventBinder() {
-        this.goalSet.addEventListener('click', this.setGoal.bind(this));
-    },
-
-    setGoal: function setGoal() {
-        this.goal.innerHTML = this.goalInput.value;
-    }
-};
-
-goalSet.init();
+alert(sup.setGoal);
